8df75f470039362291e436785c15a0cc49eff866
Justin McKenzie <mckenjus@gmail.com>
2021-09-10 09:18:54 -0600

    fourth try

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-const port = process.env.port || 4005   
+const port = process.env.PORT || 4005   
 
 
 app.listen(port, () => {
