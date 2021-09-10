63b209c4e93bd7241d1c76b665954cd44bc535ec
Justin McKenzie <mckenjus@gmail.com>
2021-09-10 08:42:51 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express ()
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+})
+
+const port = process.env.port || 4005   
+
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
